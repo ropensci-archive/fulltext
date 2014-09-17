6de3ce8fc2e4095b751c40ca8b634b356dc05e3e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-17 00:45:59 -0700

    fixed up ft_get to work with elife examples and code

--- R/ft_get.R
+++ R/ft_get.R
@@ -1,34 +1,45 @@
 #' Get full text.
 #' 
 #' @export
-#' @import rplos bmc
+#' @import rplos bmc 
+#' @importFrom elife elife_paper
 #' @importFrom rentrez entrez_search entrez_fetch
 #' 
-#' @param query Query terms
 #' @param ids Identifiers for papers, either DOIs, or other ids.
+#' @param query Query terms
 #' @param from Source to query
-#' @param limit Number of records to return.
 #' @param plosopts PLOS options. See \code{?searchplos}
 #' @param bmcopts BMC options. See \code{?bmc_search}
 #' @param entrezopts Entrez options. See \code{?entrez_search} and \code{?entrez_fetch}
+#' @param elifeopts eLife options. See \code{?elife_search}
 #' @param ... Further args passed on to \code{httr::GET}
 #' 
 #' @return An object of class ft_data
 #'
 #' @examples \donttest{
-#' ft_get(ids='10.1371%2Fjournal.pone.0086169', from='plos')
+#' ft_get(ids='10.1371/journal.pone.0086169', from='plos')
 #' (dois <- searchplos(q="*:*", fl='id', fq='doc_type:full', limit=5)$data$id)
 #' ft_get(ids=dois, from='plos')
 #' ft_get(ids=c('10.7717/peerj.228','10.7717/peerj.234'), from='entrez')
+#' 
+#' # elife
+#' ft_get(ids=c('10.7554/eLife.04300','10.7554/eLife.03032'), from='elife')
+#' library('elife')
+#' dois <- searchelife(terms="Cell biology", searchin="subject_area", boolean="contains")
+#' ft_get(ids=dois[1:10], from='elife')
+#' 
+#' # bmc
 #' ft_get(ids='http://www.microbiomejournal.com/content/download/xml/2049-2618-2-7.xml', from='bmc')
 #' }
 
-ft_get <- function(ids, query, from='plos', plosopts=list(), bmcopts=list(), entrezopts=list(), ...)
+ft_get <- function(ids, query, from='plos', plosopts=list(), bmcopts=list(), entrezopts=list(), 
+                   elifeopts=list(), ...)
 {
-  plos_out <- plugin_get_plos(from, ids, limit, plosopts, ...)
-  entrez_out <- plugin_get_entrez(from, ids, limit, entrezopts, ...)
-  bmc_out <- plugin_get_bmc(from, ids, limit, bmcopts, ...)
-  res <- list(plos=plos_out, entrez=entrez_out, bmc=bmc_out)
+  plos_out <- plugin_get_plos(from, ids, plosopts, ...)
+  entrez_out <- plugin_get_entrez(from, ids, entrezopts, ...)
+  bmc_out <- plugin_get_bmc(from, ids, bmcopts, ...)
+  elife_out <- plugin_get_elife(from, ids, elifeopts, ...)
+  res <- list(plos=plos_out, entrez=entrez_out, bmc=bmc_out, elife=elife_out)
   class(res) <- "ft_data"
   res
 }
@@ -52,7 +63,7 @@ print.ft_data <- function(x, ...) {
   cat("NOTE: extract xml strings like output$source['<doi>']")
 }
 
-plugin_get_plos <- function(sources, ids, limit, opts, ...){
+plugin_get_plos <- function(sources, ids, opts, ...){
   callopts <- list(...)
   if(any(grepl("plos", sources))){
     opts$doi <- ids
@@ -65,7 +76,7 @@ plugin_get_plos <- function(sources, ids, limit, opts, ...){
   }
 }
 
-plugin_get_entrez <- function(sources, ids, limit, opts, ...){
+plugin_get_entrez <- function(sources, ids, opts, ...){
   callopts <- list(...)
   if(any(grepl("entrez", sources))){
     opts$ids <- ids
@@ -82,7 +93,7 @@ entrez_get <- function(ids){
   vapply(res$ids, function(z) entrez_fetch(db = 'pmc', id=z, rettype = "xml"), character(1))
 }
 
-plugin_get_bmc <- function(sources, query, limit, opts, ...){
+plugin_get_bmc <- function(sources, query, opts, ...){
   callopts <- list(...)
   if(any(grepl("bmc", sources))){
     opts$uris <- query
@@ -90,6 +101,20 @@ plugin_get_bmc <- function(sources, query, limit, opts, ...){
     out <- do.call(bmc_xml, opts)
     attr(out, "format") <- "xml"
     list(found = length(out), data = out, opts = opts)
+  } else {
+    list(found = NULL, data = NULL, opts = opts)
+  }
+}
+
+plugin_get_elife <- function(sources, ids, opts, ...){
+  callopts <- list(...)
+  if(any(grepl("elife", sources))){
+    opts$doi <- ids
+    opts$what <- "text"
+    out2 <- lapply(ids, elife_paper, what="text")
+    names(out2) <- ids
+    attr(out2, "format") <- "xml"
+    list(found = length(out2), data = out2, opts = opts)
   } else {
     list(found = NULL, data = NULL, opts = opts)
   }
