94b2155b6d752d639cd90b8fd00b54be207696dc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-29 16:47:40 -0700

    added collect test, fix #35

--- R/collect.R
+++ R/collect.R
@@ -9,22 +9,27 @@
 #' x %>% collect()
 #' x %>% collect() %>% text()
 #' }
-collect <- function(x, ...) UseMethod("collect")
+collect <- function(x, ...) {
+  UseMethod("collect")
+}
 
 #' @export
 #' @rdname collect
-collect.ft_data <- function(x, ...){
-  for(i in seq_along(x)){
-    x[[i]]$data$data <- cache_get(key=x[[i]]$data$path, 
-                                  backend=cache_options_get()$backend, 
-                                  path=cache_options_get()$path)
+collect.ft_data <- function(x, ...) {
+  for (i in seq_along(x)) {
+    x[[i]]$data$data <- cache_get(key = x[[i]]$data$path, 
+                                  backend = cache_options_get()$backend, 
+                                  path = cache_options_get()$path)
   }
   x
 }
 
-text <- function(x, ...) UseMethod("text")
-text.ft_data <- function(x, ...){
-  lapply(x, function(z){
+text <- function(x, ...) {
+  UseMethod("text")
+}
+
+text.ft_data <- function(x, ...) {
+  lapply(x, function(z) {
     unclass(z$data$data)
   })
 }
--- tests/testthat/test-ft_collect.R
+++ tests/testthat/test-ft_collect.R
@@ -1,68 +1,33 @@
-context("ft_serialize")
+context("collect")
 
-test_that("ft_serialize returns...", {
+test_that("collect returns...", {
   skip_on_cran()
 
-  dois <- c('10.1371/journal.pone.0087376','10.1371%2Fjournal.pone.0086169',
-  '10.1371/journal.pone.0102976','10.1371/journal.pone.0105225',
-  '10.1371/journal.pone.0102722','10.1371/journal.pone.0033693')
-  res <- ft_get(dois, from='plos')
-
-  # From XML to JSON
-  tojson <- ft_serialize(res, to='json')
-  tojsonparsed <- jsonlite::fromJSON(tojson$plos$data$data$`10.1371/journal.pone.0087376`)
-
-  # Parse raw xml to XMLInternalDocument class
-  toxml <- ft_serialize(res, to='xml')
-  toxmlparsed <- toxml$plos$data$data[[1]]
-
-  # To a list
-  tolist <- ft_serialize(res, to='list')
-  tolistparsed <- tolist$plos$data$data[[1]]
-
-  # To various data stores on disk
-  ## To an .Rds file
-  tofile <- ft_serialize(res, to = 'file')
-  tofileloc <- attr(tofile, "location")
-
-  ## To local files using R.cache package
-  torcache <- ft_serialize(res, to = 'rcache')
-  torcacheloc <- attr(torcache, "location")
+  res <- ft_get('10.1371/journal.pone.0086169', from='plos', cache=TRUE, backend="rds")
+  aa <- x %>% collect()
+  bb <- x %>% collect() %>% text()
 
   # correct classes
-  expect_is(tojson, "ft_parsed")
-  expect_is(toxml, "ft_parsed")
-  expect_is(tolist, "ft_parsed")
-  expect_is(tofile, "ft_parsed")
-  expect_is(torcache, "ft_parsed")
+  expect_is(res, "ft_data")
+  expect_is(aa, "ft_data")
+  expect_is(bb, "list")
+  
+  expect_is(aa$plos, "list")
+  expect_is(res$plos$data, "list")
+  expect_is(aa$plos$data, "list")
+  expect_null(res$plos$data$data)
+  expect_is(aa$plos$data$data, "plosft")
 
-  expect_is(tojson$plos, "list")
-  expect_is(tojson$plos$found, "integer")
-  expect_is(tojson$plos$dois, "character")
-  expect_is(tojson$plos$opts, "list")
-  expect_is(tojson$plos$data, "list")
-  expect_is(tojson$plos$data$data, "plosft")
-  expect_is(tojson$plos$data$data[[1]], "json")
-
-  expect_is(tojsonparsed, "list")
-  expect_is(toxmlparsed, "XMLInternalDocument")
-  expect_is(tolistparsed, "list")
-  expect_is(tofileloc, "character")
-  expect_is(torcacheloc, "character")
-
-  expect_match(tofileloc, "fulltext_cache")
-  expect_match(torcacheloc, "Rcache")
+  expect_is(bb$plos, "list")
+  expect_is(bb$plos[[1]], "character")
 })
 
-test_that("ft_serialize fails well", {
+test_that("collect fails well", {
   skip_on_cran()
 
-  res <- ft_get('10.1371/journal.pone.0087376', from='plos')
+  res <- ft_get('10.1371/journal.pone.0086169', from='plos', cache=TRUE, backend="rds")
 
-  # bad path given
-  expect_error(ft_serialize(), "\"x\" is missing")
-  expect_error(ft_serialize('adfafsdf'), "Input to x must be of class ft_data")
-  expect_error(ft_serialize(5), "Input to x must be of class ft_data")
-  expect_error(ft_serialize(mtcars), "Input to x must be of class ft_data")
-  expect_error(ft_serialize(res, "nada"), "'arg' should be one of")
+  expect_error(collect())
+  expect_error(collect('adfafsdf'), "no applicable method")
+  expect_error(collect(5), "no applicable method")
 })
