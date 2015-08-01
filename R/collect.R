cf199905d58f3e3ed7e60b3dd3a5c262a36b46fc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-31 17:42:14 -0700

    fixes to collect(), changed text() to get_text() to avoid base fxn conflict

--- R/collect.R
+++ R/collect.R
@@ -1,13 +1,28 @@
 #' Collect data from a remote source in fulltext
 #' 
+#' \code{collect} grabs full text data from a remote storage device. \code{get_text} 
+#' is a convenience function to grab the nested text data and bring it up in the 
+#' list for easier access
+#' 
 #' @name collect
 #' @export
-#' @param x Input.
+#' @param x Input. An object of class \code{ft_data}
 #' @param ... Further args, ignored.
 #' @examples \dontrun{
+#' # Get some data, stash in rds file
 #' x <- ft_get('10.1371/journal.pone.0086169', from='plos', cache=TRUE, backend="rds")
-#' x %>% collect()
-#' x %>% collect() %>% text()
+#' 
+#' # note that the data is not in the object, gives NULL
+#' x$plos$data$data
+#' 
+#' # Collect data from the rds file
+#' y <- x %>% collect()
+#' 
+#' # note how the data is now in the object
+#' y$plos$data$data
+#' 
+#' # Let's get the actual 
+#' x %>% collect() %>% get_text()
 #' }
 collect <- function(x, ...) {
   UseMethod("collect")
@@ -24,11 +39,15 @@ collect.ft_data <- function(x, ...) {
   x
 }
 
-text <- function(x, ...) {
-  UseMethod("text")
+#' @export
+#' @rdname collect
+get_text <- function(x, ...) {
+  UseMethod("get_text")
 }
 
-text.ft_data <- function(x, ...) {
+#' @export
+#' @rdname collect
+get_text.ft_data <- function(x, ...) {
   lapply(x, function(z) {
     unclass(z$data$data)
   })
