0ceeb37db12505bed62b0299378ead9138549a56
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-28 18:50:40 -0700

    lowercase all putputs from ft_search() fix #63

--- R/plugins_search.R
+++ R/plugins_search.R
@@ -11,6 +11,7 @@ plugin_plos <- function(sources, query, limit, opts){
                               unrestricted use, distribution, and reproduction in any medium,
                               provided the original author and source are credited.'))
     if (!is(zz$data, "data.frame")) zz$data <- data.frame(NULL)
+    zz <- names_lower(zz)
     structure(zz, class = "ft_ind", query = query)
   } else {
     zz <- list(found = NULL, data = NULL, opts = opts)
@@ -26,6 +27,7 @@ plugin_crossref <- function(sources, query, limit, opts){
     # opts$filter <- c(`license.url`='http://creativecommons.org/licenses/by/3.0/deed.en_US')
     out <- tryCatch(do.call(cr_works, opts), warning = function(w) w)
     if (is(out, "simpleWarning")) stop(out$message, call. = FALSE)
+    out$data <- names_lower(out$data)
     zz <- list(found = out$meta$total_results, data = out$data, opts = opts, 
                license = list(type = "variable, see individual records"))
     structure(zz, class = "ft_ind", query = query)
@@ -41,6 +43,7 @@ plugin_bmc <- function(sources, query, limit, opts){
     opts$limit <- limit
     out <- do.call(bmc_search, opts)
     dat <- do.call(rbind, lapply(out$results$entries, data.frame, stringsAsFactors = FALSE))
+    dat <- names_lower(dat)
     opts$query <- opts$terms; opts$terms <- NULL
     zz <- list(found = NA, data = dat, opts = opts, 
                license = list(type = "variable, see `isOpenAccess` field in results"))
@@ -71,6 +74,7 @@ plugin_entrez <- function(sources, query, limit, opts){
     row.names(data) <- NULL
     data <- move_col(data, "title")
     data <- move_col(data, "authors")
+    data <- names_lower(data)
 
     zz <- list(found = as.integer(out$count), data = data, opts = opts)
     structure(zz, class = "ft_ind", query = query)
@@ -85,6 +89,7 @@ plugin_arxiv <- function(sources, query, limit, opts){
     opts$query <- query
     opts$limit <- limit
     out <- do.call(arxiv_search, opts)
+    out <- names_lower(out)
     zz <- list(found = attributes(out)$total_results, data = out, opts = opts,
                license = list(type = "variable, but should be free to text-mine, see http://arxiv.org/help/license and http://arxiv.org/help/bulk_data"))
     structure(zz, class = "ft_ind", query = query)
@@ -99,6 +104,7 @@ plugin_biorxivr <- function(sources, query, limit, opts){
     opts$query <- query
     opts$limit <- limit
     out <- do.call(biorxiv_search, opts)
+    out$data <- names_lower(out$data)
     zz <- list(found = out$found, data = out$data, opts = opts, 
                license = list(type = "variable, but free to text-mine, see http://biorxiv.org/about-biorxiv"))
     structure(zz, class = "ft_ind", query = query)
@@ -106,7 +112,4 @@ plugin_biorxivr <- function(sources, query, limit, opts){
     zz <- list(found = NULL, data = NULL, opts = opts)
     structure(zz, class = "ft_ind", query = query)
   }
-
 }
-
-move_col <- function(x, y) x[ c(names(x)[-grep(y, names(x))], y) ]
