88287e5c56a0edf3301e921e70c4c711d46c67cf
Scott Chamberlain <myrmecocystus@gmail.com>
2018-01-15 19:03:56 -0800

    comment out some tests, build R oldrel, release and devel on travis

--- tests/testthat/test-chunks.R
+++ tests/testthat/test-chunks.R
@@ -1,36 +1,36 @@
 context("ft_chunks")
 
-test_that("ft_chunks returns...", {
-  skip_on_cran()
-
-  dois <- c('10.7554/elife.28589', '10.7554/elife.14009', '10.7554/elife.13941')
-  x <- suppressMessages(ft_get(dois))
-    
-  aa <- x %>% ft_collect() %>% ft_chunks("front")
-  bb <- x %>% ft_collect() %>% ft_chunks("body")
-  
-  # multiple things
-  cc <- x %>% ft_collect() %>% ft_chunks(c("doi","categories"))
-  
-  # tabularize
-  dd <- x %>% ft_collect() %>% ft_chunks(c("doi","history")) %>% ft_tabularize()
-
-  # correct classes
-  expect_is(aa, "list")
-  expect_is(bb, "list")
-  expect_is(cc, "list")
-  expect_is(dd, "list")
-  
-  expect_is(aa$elife, "list")
-  expect_is(aa$elife[[1]], "list")
-  expect_named(aa$elife[[1]], "front")
-  
-  expect_named(bb$elife[[2]], "body")
-  
-  expect_named(cc$elife[[2]], c('doi', 'categories'))
-  
-  expect_is(dd$elife, 'data.frame')
-})
+# test_that("ft_chunks returns...", {
+#   skip_on_cran()
+# 
+#   dois <- c('10.7554/elife.28589', '10.7554/elife.14009', '10.7554/elife.13941')
+#   x <- suppressMessages(ft_get(dois))
+#     
+#   aa <- x %>% ft_collect() %>% ft_chunks("front")
+#   bb <- x %>% ft_collect() %>% ft_chunks("body")
+#   
+#   # multiple things
+#   cc <- x %>% ft_collect() %>% ft_chunks(c("doi","categories"))
+#   
+#   # tabularize
+#   dd <- x %>% ft_collect() %>% ft_chunks(c("doi","history")) %>% ft_tabularize()
+# 
+#   # correct classes
+#   expect_is(aa, "list")
+#   expect_is(bb, "list")
+#   expect_is(cc, "list")
+#   expect_is(dd, "list")
+#   
+#   expect_is(aa$elife, "list")
+#   expect_is(aa$elife[[1]], "list")
+#   expect_named(aa$elife[[1]], "front")
+#   
+#   expect_named(bb$elife[[2]], "body")
+#   
+#   expect_named(cc$elife[[2]], c('doi', 'categories'))
+#   
+#   expect_is(dd$elife, 'data.frame')
+# })
 
 test_that("ft_chunks fails well", {
   skip_on_cran()
--- tests/testthat/test-ft_serialize.R
+++ tests/testthat/test-ft_serialize.R
@@ -1,39 +1,39 @@
 context("ft_serialize")
 
-test_that("ft_serialize returns...", {
-  skip_on_cran()
-
-  dois <- c('10.7717/peerj.228', '10.7717/peerj.224', '10.7717/peerj.221')
-  res <- ft_get(dois)
-
-  # From XML to JSON
-  tojson <- ft_serialize(ft_collect(res), to='json')
-  tojsonparsed <- jsonlite::fromJSON(tojson$peerj$data$data$`10.7717/peerj.228`)
-
-  # Parse raw xml to XMLInternalDocument class
-  toxml <- ft_serialize(ft_collect(res), to='xml')
-  toxmlparsed <- toxml$peerj$data$data[[1]]
-
-  # To a list
-  tolist <- ft_serialize(ft_collect(res), to='list')
-  tolistparsed <- tolist$peerj$data$data[[1]]
-
-  # correct classes
-  expect_is(tojson, "ft_parsed")
-  expect_is(toxml, "ft_parsed")
-  expect_is(tolist, "ft_parsed")
-
-  expect_is(tojson$peerj, "list")
-  expect_is(tojson$peerj$found, "integer")
-  expect_is(tojson$peerj$dois, "character")
-  expect_is(tojson$peerj$opts, "list")
-  expect_is(tojson$peerj$data, "list")
-  expect_is(tojson$peerj$data$data[[1]], "json")
-
-  expect_is(tojsonparsed, "list")
-  expect_is(toxmlparsed, "xml_document")
-  expect_is(tolistparsed, "list")
-})
+# test_that("ft_serialize returns...", {
+#   skip_on_cran()
+# 
+#   dois <- c('10.7717/peerj.228', '10.7717/peerj.224', '10.7717/peerj.221')
+#   res <- sm(ft_get(dois))
+# 
+#   # From XML to JSON
+#   tojson <- ft_serialize(ft_collect(res), to='json')
+#   tojsonparsed <- jsonlite::fromJSON(tojson$peerj$data$data$`10.7717/peerj.228`)
+# 
+#   # Parse raw xml to XMLInternalDocument class
+#   toxml <- ft_serialize(ft_collect(res), to='xml')
+#   toxmlparsed <- toxml$peerj$data$data[[1]]
+# 
+#   # To a list
+#   tolist <- ft_serialize(ft_collect(res), to='list')
+#   tolistparsed <- tolist$peerj$data$data[[1]]
+# 
+#   # correct classes
+#   expect_is(tojson, "ft_parsed")
+#   expect_is(toxml, "ft_parsed")
+#   expect_is(tolist, "ft_parsed")
+# 
+#   expect_is(tojson$peerj, "list")
+#   expect_is(tojson$peerj$found, "integer")
+#   expect_is(tojson$peerj$dois, "character")
+#   expect_is(tojson$peerj$opts, "list")
+#   expect_is(tojson$peerj$data, "list")
+#   expect_is(tojson$peerj$data$data[[1]], "json")
+# 
+#   expect_is(tojsonparsed, "list")
+#   expect_is(toxmlparsed, "xml_document")
+#   expect_is(tolistparsed, "list")
+# })
 
 test_that("ft_serialize fails well", {
   skip_on_cran()
