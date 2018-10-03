87025a6418c0eb38d6ce906a282b63103ad0b194
Scott Chamberlain <myrmecocystus@gmail.com>
2018-10-02 18:41:03 -0700

    fix #173 fix to ft_links that was causing issue when passed to ft_get depends on dev version of crminer that has a fix in crm_links

---
+++ tests/testthat/test-cache_file_info.R
@@ -0,0 +1,38 @@
+context("cache_file_info")
+
+# delete all files
+ftxt_cache$delete_all()
+
+test_that("cache_file_info - when cache dir empty", {
+  skip_on_cran()
+  
+  aa <- cache_file_info()
+  
+  expect_is(aa, "list")
+  expect_named(aa, c('xml_not_valid', 'xml_abstract_only', 'pdf_not_valid'))
+  expect_equal(length(aa$xml_not_valid), 0)
+  expect_equal(length(aa$xml_abstract_only), 0)
+  expect_equal(length(aa$pdf_not_valid), 0)
+})
+
+test_that("cache_file_info - when cache dir not empty", {
+  skip_on_cran()
+  
+  # download a file
+  pap <- ft_get('10.1371/journal.pone.0086169')
+
+  # cache info
+  aa <- cache_file_info()
+  
+  expect_is(aa, "list")
+  expect_named(aa, c('xml_not_valid', 'xml_abstract_only', 'pdf_not_valid'))
+  expect_equal(length(aa$xml_not_valid), 0)
+  expect_equal(length(aa$xml_abstract_only), 0)
+  expect_equal(length(aa$pdf_not_valid), 0)
+})
+
+test_that("cache_file_info fails well", {
+  skip_on_cran()
+  
+  expect_error(cache_file_info(5), "unused argument")
+})
