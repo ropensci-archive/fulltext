2e71c78a7559380026ac680dafcab99b8b283283
Scott Chamberlain <myrmecocystus@gmail.com>
2020-10-16 16:14:36 -0700

    remove some unused code

--- R/ftdoi_other.R
+++ R/ftdoi_other.R
@@ -78,8 +78,9 @@ a_prefix <- function(id) {
 #' @param doi (characte) a doi
 #' @return a named list with: prefix, member, name
 #' @family ftdoi
-#' @examples
+#' @examples \dontrun{
 #' prefix_local('10.3390/ani4010082')
+#' }
 prefix_local <- function(doi) {
   prefix <- doi_prefix(doi)
   z <- crossref_member_prefix[crossref_member_prefix$prefixes %in% prefix,]
--- R/ftdoi_storr_funs.R
+++ R/ftdoi_storr_funs.R
@@ -78,22 +78,3 @@ storr_init <- function(x) {
 }
 
 ftdoi_doi_prefixes <- ftdoi_urls <- ftdoi_doi_issn <- NULL
-
-# Initialize cache folders for ftdoi
-# ftdoi_cache_init <- function() {
-#   if (!is.null(ftdoi_doi_prefixes)) {
-#     ftdoi_doi_prefixes <<- storr::storr_rds(
-#       file.path(ftdoi_cache$cache_path_get(), "_doi_prefixes"),
-#       mangle_key = TRUE)
-#   }
-#   if (!is.null(ftdoi_urls)) {
-#     ftdoi_urls <<- storr::storr_rds(
-#       file.path(ftdoi_cache$cache_path_get(), "_urls"),
-#       mangle_key = TRUE)
-#   }
-#   if (!is.null(ftdoi_doi_issn)) {
-#     ftdoi_doi_issn <<- storr::storr_rds(
-#       file.path(ftdoi_cache$cache_path_get(), "_doi_issn"),
-#       mangle_key = TRUE)
-#   }
-# }
